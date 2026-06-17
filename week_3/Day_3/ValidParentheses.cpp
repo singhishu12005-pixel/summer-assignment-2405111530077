d10c2f4d73072de072d534a69715fbeac8f76b10
ISHITA SINGH <singhishu12005@gmail.com>
2026-06-17 16:48:27 +0530

    Create ValidParentheses.cpp

---
+++ week_3/Day_3/ValidParentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c : s) {
+            if(c=='(' || c=='{' || c=='[') {
+                st.push(c);
+            } else {
+                if(st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if((top=='(' && c!=')') ||
+                   (top=='{' && c!='}') ||
+                   (top=='[' && c!=']'))
+                    return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
