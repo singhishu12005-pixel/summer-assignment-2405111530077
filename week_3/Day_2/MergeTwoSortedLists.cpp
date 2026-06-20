6224ac39f84819faf17e972e27d796de9d77011a
ISHITA SINGH <singhishu12005@gmail.com>
2026-06-20 23:31:29 +0530

    Create MergeTwoSortedLists.cpp

---
+++ week_3/Day_2/MergeTwoSortedLists.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+
+        ListNode* head = new ListNode(0);
+        ListNode* temp = head;
+
+        while(list1 && list2)
+        {
+            if(list1->val < list2->val)
+            {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+
+            temp = temp->next;
+        }
+
+        if(list1)
+        {
+            temp->next = list1;
+        }
+        else
+        {
+            temp->next = list2;
+        }
+
+        return head->next;
+    }
+};
