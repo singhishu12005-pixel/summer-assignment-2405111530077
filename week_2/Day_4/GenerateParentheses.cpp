3223d186fe578e49a4050390fc00b51e6670ff48
ISHITA SINGH <singhishu12005@gmail.com>
2026-06-22 12:18:06 +0530

    Create GenerateParentheses.cpp

---
+++ week_2/Day_4/GenerateParentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> result;
+
+    void backtrack(string current, int open, int close, int n) {
+        if (current.length() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(current + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(current + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return result;
+    }
+};
